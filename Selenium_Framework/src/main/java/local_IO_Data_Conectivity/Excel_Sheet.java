b0735cc41848c92e2e67a8104f92374095d684e2
Mfaisal <mfaisalkemary@gmail.com>
2017-04-15 17:24:19 +0300

    Building the project structure

---
+++ Selenium_Framework/src/main/java/local_IO_Data_Conectivity/Excel_Sheet.java
@@ -0,0 +1,25 @@
+package local_IO_Data_Conectivity;
+
+public class Excel_Sheet {
+
+	/*this method is to return data from the excel sheet based on the 
+	provided column , row numbers */ 
+	public  Object Get_Data (int col_num,int row_num){
+		
+	}
+	
+	/*this method is to write data to an excel sheet based on the provided 
+	 * column , row numbers*/
+	public void Set_Data(Object obj,int col_num,int row_num){
+		
+	}
+	
+	/*this method is to return a row from the excel sheet based on the 
+	 * value of a cell in the sheet*/
+	
+	public  String[] Control(int row_num,int col_num,boolean control){
+		
+	}
+	
+}
+
