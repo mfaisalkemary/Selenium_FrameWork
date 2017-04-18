7f78d9c9462ad82bc2cf79a3c3544d76f2a730cc
Mkamary <Mkamary@192.168.8.103>
2017-04-18 10:51:32 +0300

    new

--- Selenium_Framework/src/main/java/local_IO_Data_Conectivity/Excel_Sheet.java
+++ Selenium_Framework/src/main/java/local_IO_Data_Conectivity/Excel_Sheet.java
@@ -5,7 +5,7 @@ public class Excel_Sheet {
 	/*this method is to return data from the excel sheet based on the 
 	provided column , row numbers */ 
 	public  Object Get_Data (int col_num,int row_num){
-		wdwdw
+		return "cocowawa";
 	}
 	
 	/*this method is to write data to an excel sheet based on the provided 
@@ -18,7 +18,7 @@ public class Excel_Sheet {
 	 * value of a cell in the sheet*/
 	
 	public  String[] Control(int row_num,int col_num,boolean control){
-		
+		return null;
 	}
 	
 }
